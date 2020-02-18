fe7701a14f7bb0c501a8788a6b6e506e742f4325
Jason <jasylwong@gmail.com>
2020-02-18 16:44:23 +0000

    add usage indicator;

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -15,20 +15,37 @@ describe('Thermostat', function() {
     it('switches power saving mode from on to off', function() {
       thermostat.mode_switcher();
       expect(thermostat.power_saving_mode).toBe(false);
-    })
+    });
 
     it('switches power saving mode from off to on', function() {
       thermostat.mode_switcher();
       expect(thermostat.power_saving_mode).toBe(false);
       thermostat.mode_switcher();
       expect(thermostat.power_saving_mode).toBe(true);
+    });
+
+    it('sets the maximum at 32 if off', function() {
+      thermostat.mode_switcher();
+      expect(thermostat.power_saving_mode).toBe(false);
+      thermostat.temperature = 32;
+      expect(thermostat.current_temp()).toEqual(32);
+      thermostat.increase_temp();
+      expect(thermostat.current_temp()).toEqual(32);
+    });
+  });
+
+  describe('reset', function() {
+    it('resets the temperature to 20', function() {
+      thermostat.temperature = 25;
+      expect(thermostat.current_temp()).toEqual(25);
+      thermostat.reset();
+      expect(thermostat.current_temp()).toEqual(20);
     })
   })
 
-
   describe('temperature', function() {
-    it('starts at twenty degrees', function() {
-      expect(thermostat.temperature).toEqual(20)
+    it('starts at default', function() {
+      expect(thermostat.temperature).toEqual(thermostat.default_temp)
     });
 
     it('has a default min of 10', function() {
@@ -71,6 +88,24 @@ describe('Thermostat', function() {
       expect(thermostat.current_temp()).toEqual(10);
       thermostat.decrease_temp();
       expect(thermostat.current_temp()).toEqual(10);
+    });
+  });
+
+  describe('usage_indicator', function() {
+    it('shows "medium usage" when 18 <= temp < 25', function() {
+      expect(thermostat.usage_indicator()).toEqual('medium-usage');
+    });
+
+    it('shows "low usage" when temp < 18', function() {
+      thermostat.temperature = 10;
+      expect(thermostat.current_temp()).toEqual(10);
+      expect(thermostat.usage_indicator()).toEqual('low-usage');
+    });
+
+    it('shows "high usage" when temp > 25', function() {
+      thermostat.temperature = 25;
+      expect(thermostat.current_temp()).toEqual(25);
+      expect(thermostat.usage_indicator()).toEqual('high-usage');
     });
   });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,19 +1,20 @@
 function Thermostat() {
-  this.temperature = 20;
+  this.default_temp = 20;
+  this.temperature = this.default_temp;
   this.min_temp = 10;
   this.max_temp = 25;
+  this.max_temp_mode_off = 32;
   this.power_saving_mode = true;
 }
 
 var thermostat = new Thermostat;
 
 Thermostat.prototype.mode_switcher = function() {
-  // if (this.power_saving_mode === true) {
-  //   this.power_saving_mode = false;
-  // } else {
-  //   this.power_saving_mode = true;
-  // }
   this.power_saving_mode = (this.power_saving_mode ? false : true); 
+}
+
+Thermostat.prototype.reset = function() {
+  this.temperature = 20;
 }
 
 Thermostat.prototype.current_temp = function() {
@@ -21,11 +22,25 @@ Thermostat.prototype.current_temp = function() {
 }
 
 Thermostat.prototype.increase_temp = function() {
-  if (this.current_temp() < this.max_temp) { this.temperature++ };
+  if ( this.power_saving_mode === true) {
+    if (this.current_temp() < this.max_temp) { this.temperature++ };
+  } else {
+    if (this.current_temp() < this.max_temp_mode_off) { this.temperature++ };
+  }
 }
 
 Thermostat.prototype.decrease_temp = function() {
   if (this.current_temp() > this.min_temp) { this.temperature-- };
 }
+
+Thermostat.prototype.usage_indicator = function() {
+  if (this.current_temp() >= 18 && this.current_temp() < 25) {
+    return 'medium-usage';
+  } else if (this.current_temp() < 18) {
+    return 'low-usage';
+  } else {
+    return 'high-usage';
+  };
+};
 
 console.log(thermostat.current_temp());
