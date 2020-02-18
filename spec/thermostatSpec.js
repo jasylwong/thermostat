b14a76a1cd1d8126c98347ee3b62529f82a673cf
Jason <jasylwong@gmail.com>
2020-02-18 15:53:35 +0000

    switch power saving mode

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -7,9 +7,70 @@ describe('Thermostat', function() {
     thermostat = new Thermostat();
   })
 
-  describe('start_temp', function() {
-    it('starts at twenty', function() {
-      expect(thermostat.start_temp).toEqual(20)
+  describe('power saving mode', function() {
+    it('defaults with power saving mode on', function() {
+      expect(thermostat.power_saving_mode).toBe(true);
+    });
+  
+    it('switches power saving mode from on to off', function() {
+      thermostat.mode_switcher();
+      expect(thermostat.power_saving_mode).toBe(false);
+    })
+
+    it('switches power saving mode from off to on', function() {
+      thermostat.mode_switcher();
+      expect(thermostat.power_saving_mode).toBe(false);
+      thermostat.mode_switcher();
+      expect(thermostat.power_saving_mode).toBe(true);
+    })
+  })
+
+
+  describe('temperature', function() {
+    it('starts at twenty degrees', function() {
+      expect(thermostat.temperature).toEqual(20)
+    });
+
+    it('has a default min of 10', function() {
+      expect(thermostat.min_temp).toEqual(10);
+    });
+
+    it('has a default max of 10', function() {
+      expect(thermostat.max_temp).toEqual(25);
+    });
+  });
+
+  describe('current_temp', function() {
+    it('returns the current temp', function() {
+      expect(thermostat.current_temp()).toEqual(20)
+    });
+  });
+
+  describe('increase_temp', function() {
+    it('increases the temp by one', function() {
+      thermostat.increase_temp();
+      expect(thermostat.current_temp()).toEqual(21);
+    });
+
+    it('has a max of 25 degrees when power saving mode on', function() {
+      thermostat.temperature = 25;
+      expect(thermostat.current_temp()).toEqual(25);
+      thermostat.increase_temp();
+      expect(thermostat.current_temp()).toEqual(25);
+    });
+  });
+
+  describe('decrease_temp', function() {
+    it('decreases the temp by one', function() {
+      thermostat.decrease_temp();
+      expect(thermostat.current_temp()).toEqual(19);
+    });
+
+    it('has a minimum of 10 degrees', function() {
+      thermostat.temperature = 10;
+      expect(thermostat.current_temp()).toEqual(10);
+      thermostat.decrease_temp();
+      expect(thermostat.current_temp()).toEqual(10);
     });
   });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,5 +1,31 @@
 function Thermostat() {
-  this.start_temp = 20;
+  this.temperature = 20;
+  this.min_temp = 10;
+  this.max_temp = 25;
+  this.power_saving_mode = true;
 }
 
-// var thermostat = new Thermostat;
+var thermostat = new Thermostat;
+
+Thermostat.prototype.mode_switcher = function() {
+  // if (this.power_saving_mode === true) {
+  //   this.power_saving_mode = false;
+  // } else {
+  //   this.power_saving_mode = true;
+  // }
+  this.power_saving_mode = (this.power_saving_mode ? false : true); 
+}
+
+Thermostat.prototype.current_temp = function() {
+  return this.temperature;
+}
+
+Thermostat.prototype.increase_temp = function() {
+  if (this.current_temp() < this.max_temp) { this.temperature++ };
+}
+
+Thermostat.prototype.decrease_temp = function() {
+  if (this.current_temp() > this.min_temp) { this.temperature-- };
+}
+
+console.log(thermostat.current_temp());
